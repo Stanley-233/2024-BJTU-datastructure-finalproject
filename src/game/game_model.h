a296fe5aa35e7b3c609bacc07bf192f2e6b76fe6
54shitaimzf <mzfshitiancai@126.com>
2024-12-13 11:45:45 +0800

    feat: completing game model

--- src/game/game_model.h
+++ src/game/game_model.h
@@ -31,7 +31,7 @@ enum GameLevel
     HARD
 };
 
-// 用于绘制线段的连接点
+// 用于绘制线段的连接点(其实应该统一用这个结构体的，代码有待优化)
 struct PaintPoint
 {
     PaintPoint(int _x, int _y) : x(_x), y (_y) {}
@@ -40,5 +40,66 @@ struct PaintPoint
 };
 
 // -------------------------------- //
+
+class GameModel
+{
+public:
+    GameModel();
+    virtual ~GameModel();
+public:
+    void startGame(); // 开始游戏
+    void startGame(GameLevel level);
+    int *getGameMap();    // 获得地图
+
+           //未使用
+    GameStatus checkGameStatus(); // 判断获得游戏状态，是否输赢
+
+    bool linkTwoTiles(int srcX, int srcY, int dstX,int dstY); // 连接起点和终点方块，连接是否成功
+    bool isFrozen(); // 判断是否已经成为了僵局
+    bool isWin(); // 检查游戏是否结束
+    int *getHint(); // 获得提示
+    std::vector<PaintPoint> paintPoints; // 用于绘制的点
+
+           // 游戏状态和难度
+    GameStatus gameStatus;
+    GameLevel gameLevel;
+
+    int getScore();
+    void setScore(int score);
+
+    int getTimes();
+    void setTimes(int times);
+
+    int getHintsLast();
+    void setHintsLast(int hintsLast);
+
+           //打乱剩余方块
+    void reset();
+
+private:
+    // 游戏地图，存储方块，0表示消失，1-其他数字表示图片标号
+    int *gameMap;
+
+    int score;
+
+    int hintsLast;
+    //记录消去方块数
+    int times = 0;
+
+           // 游戏提示，存储2个点
+    int *hintArray;
+
+           // 判断起点到终点的方块是否可以连接消除
+    bool isCanLink(int srcX, int srcY, int dstX, int dstY);
+
+    bool canLinkDirectly(int srcX, int srcY, int dstX, int dstY);
+    bool canLinkWithOneCorner(int srcX, int srcY, int dstX, int dstY);
+    bool canLinkWithTwoCorner(int srcX, int srcY, int dstX, int dstY);
+
+           // 提示模式还是连接模式判断
+    bool isFrozenMode;
+
+};
+
 
 #endif // GAMEMODEL_H
