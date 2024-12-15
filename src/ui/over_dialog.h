60e695e8b4d73743bb2372daf764a700e2ccaa34
54shitaimzf <mzfshitiancai@126.com>
2024-12-16 00:12:53 +0800

    feat: over dialog
    游戏结算界面类

---
+++ src/ui/over_dialog.h
@@ -0,0 +1,38 @@
+#ifndef OVERDIALOG_H
+#define OVERDIALOG_H
+
+#include <QDialog>
+#include "../game/game_model.h"
+
+namespace Ui {
+    class overDialog;
+}
+
+class overDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    overDialog(bool mode,int score,QWidget *parent = nullptr);
+    ~overDialog();
+
+    void closeEvent(QCloseEvent *e);
+
+private slots:
+    void on_exitBtn_clicked();
+
+    void on_hardBtn_clicked();
+
+    void on_basicBtn_clicked();
+
+    void on_mediumBtn_clicked();
+
+private:
+    Ui::overDialog *ui;
+
+signals:
+    void again(GameLevel mode);
+    void exit();
+};
+
+#endif // OVERDIALOG_H
